3fc154da8126a06fb7506fde7f8b5d538d261283
munetake miguchi <54785182+guchim@users.noreply.github.com>
2020-02-25 12:41:52 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,17 @@ Walfare.find_or_create_by(name:"クーポンGET")
 Walfare.find_or_create_by(name:"まかないあり")
 Walfare.find_or_create_by(name:"髪型/服装自由")
 Walfare.find_or_create_by(name:"交通費支給")
-User.create!(
-    email: "test-user@example.com",
-    phone_number: "0123111222",
-    gender: 0,
-    password: "password",
-    name: "テストユーザー",
-    birthday:"2000-01-01"
-)
-Company.create!(
-    email: "test-Company@example.com",
-    password: "adminpass",
-    name: "テストカンパニー",
-    phone_number:"00011112222"
-)
+#User.create!(
+#    email: "test-user@example.com",
+#    phone_number: "0123111222",
+#    gender: 0,
+#    password: "password",
+#    name: "テストユーザー",
+#    birthday:"2000-01-01"
+#)
+#Company.create!(
+#    email: "test-Company@example.com",
+#    password: "adminpass",
+#    name: "テストカンパニー",
+#    phone_number:"00011112222"
+#)
