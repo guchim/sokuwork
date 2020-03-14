8c4a1774c0015a950aabab67a512fbbcefb033f1
guchim <guchim1229@gmail.com>
2020-03-14 18:52:10 +0900

    capistrano導入に伴うコードの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '13.230.92.74', user: 'ec2-user', roles: %w{app db web}
+server '54.95.34.19', user: 'ec2-user', roles: %w{app db web}
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/id_aws_rsa' 
+set :ssh_options, keys: '~/.ssh/id_aws_rsa'
