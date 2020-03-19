f8e8f430a7b80a63d4fbd11b8b876453799f5336
guchim <guchim1229@gmail.com>
2020-03-19 14:04:44 +0900

    production.rbの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '54.95.34.19', user: 'ec2-user', roles: %w{app db web}
+server 'terrarform-on-docker-alb-111067836.ap-northeast-1.elb.amazonaws.com', user: 'ec2-user', roles: %w{app db web}
