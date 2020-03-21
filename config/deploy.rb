020ee56059386901bfcd1981b483bf78f970899e
guchim <guchim1229@gmail.com>
2020-03-21 14:36:19 +0900

    s3に関するファイルの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,18 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+
   desc 'Create database'
   task :db_create do
     on roles(:db) do |host|
