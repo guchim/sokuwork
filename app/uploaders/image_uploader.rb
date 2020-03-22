b74038fda3a6f1bf00c221f1e6cb374b2c0c0e83
guchim <guchim1229@gmail.com>
2020-03-22 17:57:21 +0900

    image_uploaderの編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  # 環境ごとに保存先変更/デフォルトではstorage :fileがコメントアウトで入っている
   if Rails.env.development?
     storage :file
   elsif Rails.env.test?
@@ -18,30 +19,43 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  "unknown.jpg"
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  def default_url(*args) 
+    "unknown.jpg"
   end
-　
+
   # リサイズ
-  process resize_to_limit: [100, 100]
+  process resize_to_fill: [200, 200, 'Center']
 
   # JPGで保存
   process convert: 'jpg'
 
+  # サムネイルを生成
+  #  version :thumb do
+  #    process resize_to_limit: [300, 300]
+  #  end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
+  # jpg,jpeg,gif,pngのみ
+  def extension_white_list
+    %w[jpg jpeg gif png]
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  # ファイル名を変更し拡張子を同じにする
+  def filename
+    super.chomp(File.extname(super)) + '.jpg'
+  end
+
+  # 日付で保存
+  def filename
+    if original_filename.present?
+      time = Time.now
+      name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+      name.downcase
+    end
+  end
+end
+def default_url(*args)
+#   # For Rails 3.1+ asset pipeline compatibility:
+#   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+"unknown.jpg"
+#   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
 end
